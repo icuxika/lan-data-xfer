485659b39be51c34f13910de3228091805c9c575
icuxika <icuxika@outlook.com>
2025-01-30 00:17:57 +0800

    初步支持局域网内WebRTC通信

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,185 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import {
+    computed,
+    onMounted,
+    onUnmounted,
+    ref,
+    useId,
+    type ComputedRef,
+} from "vue";
+import {
+    PeerConnectionHandler,
+    SignalingChannel,
+    type Message,
+} from "./negotiation";
+let signalingChannel: SignalingChannel;
+let currentUserId: number = 0;
+const onlineUserIdList = ref<number[]>([]);
+const onlineUserOptions: ComputedRef<
+    { id: string; text: string; value: number }[]
+> = computed(() => {
+    return onlineUserIdList.value.map((onlineUserId) => {
+        return {
+            id: useId(),
+            text:
+                onlineUserId === currentUserId
+                    ? `本机${onlineUserId}`
+                    : `其他${onlineUserId}`,
+            value: onlineUserId,
+        };
+    });
+});
+const onlineUserSelected = ref<number>(0);
+
+const map: {
+    [key: number]: PeerConnectionHandler;
+} = {};
+
+const connect = async () => {
+    const selectedUserId = onlineUserSelected.value;
+
+    const peerConnectionHandler = new PeerConnectionHandler();
+    map[onlineUserSelected.value] = peerConnectionHandler;
+    peerConnectionHandler.onicecandidate((event) => {
+        if (event.candidate) {
+            const msg: Message = {
+                senderId: currentUserId,
+                receiverId: selectedUserId,
+                content: JSON.stringify(event.candidate),
+                type: 13,
+            };
+            signalingChannel.socket.send(JSON.stringify(msg));
+        }
+    });
+    await peerConnectionHandler.initRTCPeerConnection();
+    peerConnectionHandler.onmessage((event) => {
+        onmessage(event);
+    });
+    await peerConnectionHandler.createDataChannel();
+    await peerConnectionHandler.sendDescription((description) => {
+        const message: Message = {
+            senderId: currentUserId,
+            receiverId: selectedUserId,
+            content: JSON.stringify(description),
+            type: 11,
+        };
+        signalingChannel.socket.send(JSON.stringify(message));
+    });
+};
+const sendMessage = async () => {
+    map[onlineUserSelected.value].sendText("hello");
+};
+
+const onmessage = async (event: MessageEvent) => {
+    console.log(event);
+};
+
+onMounted(() => {
+    signalingChannel = new SignalingChannel(
+        "ws://localhost:5010/websocket?token=abc&clientType=1&userId=1"
+    );
+    signalingChannel.onmessage(async (event) => {
+        const message: Message = JSON.parse(event.data);
+        switch (message.type) {
+            case 0: {
+                currentUserId = message.receiverId;
+                onlineUserIdList.value = JSON.parse(message.content);
+                console.log(
+                    `与信令服务器建立连接，得到分配的用户id为${message.receiverId}`
+                );
+                break;
+            }
+            case 1: {
+                onlineUserIdList.value = JSON.parse(message.content);
+                console.log(`在线用户列表已更新`);
+                break;
+            }
+            case 11: {
+                const description = JSON.parse(message.content);
+
+                const peerConnectionHandler = new PeerConnectionHandler();
+                map[message.senderId] = peerConnectionHandler;
+                peerConnectionHandler.onicecandidate((event) => {
+                    if (event.candidate) {
+                        const msg: Message = {
+                            senderId: currentUserId,
+                            receiverId: message.senderId,
+                            content: JSON.stringify(event.candidate),
+                            type: 13,
+                        };
+                        signalingChannel.socket.send(JSON.stringify(msg));
+                    }
+                });
+                await peerConnectionHandler.initRTCPeerConnection();
+                peerConnectionHandler.onmessage((event) => {
+                    onmessage(event);
+                });
+                await peerConnectionHandler.initOnDataChannel();
+                await peerConnectionHandler.receiveDescription(description);
+                await peerConnectionHandler.sendDescription((description) => {
+                    const msg: Message = {
+                        senderId: currentUserId,
+                        receiverId: message.senderId,
+                        content: JSON.stringify(description),
+                        type: 12,
+                    };
+                    signalingChannel.socket.send(JSON.stringify(msg));
+                });
+                break;
+            }
+            case 12: {
+                const description = JSON.parse(message.content);
+                await map[message.senderId].receiveDescription(description);
+                break;
+            }
+            case 13: {
+                const candidate = JSON.parse(message.content);
+                await map[message.senderId].addIceCandidate(candidate);
+                break;
+            }
+            default: {
+                //
+            }
+        }
+    });
+});
+onUnmounted(() => {
+    if (signalingChannel) {
+        signalingChannel.close();
+    }
+});
 </script>
 
 <template>
     <div>
-        <a href="https://vite.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
+        {{ onlineUserIdList }}
+        <select v-model="onlineUserSelected">
+            <option disabled value="0">Please select one</option>
+            <option
+                v-for="option in onlineUserOptions"
+                :value="option.value"
+                :key="option.id"
+                :disabled="option.value === currentUserId"
+            >
+                {{ option.text }}
+            </option>
+        </select>
+        {{ onlineUserSelected }}
+        <button
+            type="button"
+            @click="connect"
+            :disabled="onlineUserSelected === 0"
+        >
+            连接
+        </button>
+        <button
+            type="button"
+            @click="sendMessage"
+            :disabled="onlineUserSelected === 0"
+        >
+            发送
+        </button>
     </div>
-    <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-}
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/negotiation.ts
@@ -0,0 +1,113 @@
+class SignalingChannel {
+    socket: WebSocket;
+    url: string;
+    onmessageFunc: (event: MessageEvent) => void = () => {};
+    constructor(url: string) {
+        this.url = url;
+        this.socket = new WebSocket(url);
+        this.socket.onmessage = (event: MessageEvent) => {
+            this.onmessageFunc(event);
+        };
+    }
+    onmessage(callback: (event: MessageEvent) => void) {
+        this.onmessageFunc = callback;
+    }
+
+    close() {
+        this.socket.close();
+    }
+}
+
+interface Message {
+    senderId: number;
+    receiverId: number;
+    content: string;
+    type: number;
+}
+
+class PeerConnectionHandler {
+    pc?: RTCPeerConnection;
+    dataChannel?: RTCDataChannel;
+
+    onicecandidateFunc: (event: RTCPeerConnectionIceEvent) => void = () => {};
+    onmessageFunc: (event: MessageEvent) => void = () => {};
+    async initRTCPeerConnection(configuration: RTCConfiguration = {}) {
+        this.pc = new RTCPeerConnection(configuration);
+        this.pc.onicecandidate = (event: RTCPeerConnectionIceEvent) => {
+            this.onicecandidateFunc(event);
+        };
+    }
+
+    async initOnDataChannel() {
+        if (this.pc) {
+            this.pc.ondatachannel = (event: RTCDataChannelEvent) => {
+                this.dataChannel = event.channel;
+                this.initRTCDataChannelEvents();
+            };
+        }
+    }
+
+    async createDataChannel() {
+        if (this.pc) {
+            this.dataChannel = this.pc.createDataChannel("chat", {
+                ordered: true,
+                maxRetransmits: 10,
+            });
+            this.initRTCDataChannelEvents();
+        }
+    }
+
+    initRTCDataChannelEvents() {
+        if (this.dataChannel) {
+            this.dataChannel.onopen = () => {
+                console.log("Data channel is open");
+            };
+            this.dataChannel.onclose = () => {
+                console.log("Data channel is closed");
+            };
+            this.dataChannel.onmessage = (event: MessageEvent) => {
+                this.onmessageFunc(event);
+            };
+        }
+    }
+
+    async sendDescription(
+        callback: (description: RTCSessionDescription) => void
+    ) {
+        if (this.pc) {
+            await this.pc.setLocalDescription();
+            if (this.pc.localDescription) {
+                callback(this.pc.localDescription);
+            }
+        }
+    }
+
+    async receiveDescription(remoteDescription: RTCSessionDescription) {
+        if (this.pc) {
+            await this.pc.setRemoteDescription(remoteDescription);
+        }
+    }
+
+    async addIceCandidate(candidate: RTCIceCandidate) {
+        if (this.pc) {
+            this.pc.addIceCandidate(candidate);
+        }
+    }
+
+    onicecandidate(callback: (event: RTCPeerConnectionIceEvent) => void) {
+        this.onicecandidateFunc = callback;
+    }
+
+    onmessage(callback: (event: MessageEvent) => void) {
+        this.onmessageFunc = callback;
+    }
+
+    async sendText(data: string) {
+        if (this.dataChannel) {
+            this.dataChannel.send(data);
+        }
+    }
+}
+
+export { PeerConnectionHandler, SignalingChannel };
+export type { Message };
